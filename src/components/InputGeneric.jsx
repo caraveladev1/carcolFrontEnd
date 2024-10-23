02fa71f1d62ad38246ecda7922fbad0e081cb683
caraveladev1 <80789516+caraveladev1@users.noreply.github.com>
2024-10-23 16:09:00 -0500

    change input select

--- src/components/InputGeneric.jsx
+++ src/components/InputGeneric.jsx
@@ -3,26 +3,29 @@ import { useTranslation } from 'react-i18next';
 
 export function InputGeneric({ type = 'text', filter, defaultValue, options = [], onChange, required }) {
 	const { t } = useTranslation();
-	//console.log(defaultValue);
+
 	return (
 		<>
 			{type === 'select' ? (
-				<select
-					id={filter}
-					name={filter}
-					defaultValue={defaultValue}
-					className='bg-transparent font-bayard text-xl uppercase border-2 border-pink p-4 w-full text-pink focus:outline-none focus:border-2 focus:border-pink m-auto h-full'
-					onChange={onChange}
-					required={required}
-				>
-					<option value=''>{t('selectOption')}</option>
-					{/* Renderizar las opciones que se pasan como prop */}
-					{options.map((option, index) => (
-						<option key={index} value={option}>
-							{option}
-						</option>
-					))}
-				</select>
+				<div className='w-full'>
+					<input
+						list={`${filter}-options`}
+						id={filter}
+						name={filter}
+						defaultValue={defaultValue}
+						className='bg-transparent font-bayard text-xl uppercase border-2 border-pink p-4 w-full text-pink focus:outline-none focus:border-2 focus:border-pink m-auto h-full'
+						onChange={onChange}
+						required={required}
+						placeholder={`Select ${t(filter)}`}
+						autoComplete='off'
+					/>
+					<datalist id={`${filter}-options`}>
+						{/* Renderizar las opciones que se pasan como prop */}
+						{options.map((option, index) => (
+							<option key={index} value={option} />
+						))}
+					</datalist>
+				</div>
 			) : type === 'date' ? (
 				<input
 					type='date'
