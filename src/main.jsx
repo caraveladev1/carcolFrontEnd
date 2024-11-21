380b1e5b0a0b9a8b3d775b6c13fd0d398d60a09d
Juan Diaz <juan.diaz@caravela.coffee>
2024-11-21 12:00:54 -0500

    impolement browser router

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Home } from './pages/Home';
 import '../I18n';
-import { createHashRouter, RouterProvider, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importa BrowserRouter, Routes y Route
 import { CreateContainer } from './pages/CreateContainer';
 import { ViewContainers } from './pages/ViewContainers';
 import { EditContainer } from './pages/EditContainer';
@@ -11,60 +11,57 @@ import { ExportedContainers } from './pages/ExportedContainers';
 import { Login } from './pages/Login';
 import { ProtectedRoute } from './components/ProtectedRoute'; // Importa el componente ProtectedRoute
 
-// Definir las rutas directamente en createHashRouter
-const router = createHashRouter([
-	{
-		path: '/login',
-		element: <Login />,
-	},
-	{
-		path: '/',
-		element: (
-			<ProtectedRoute>
-				<Home />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/create',
-		element: (
-			<ProtectedRoute>
-				<CreateContainer />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/view-containers',
-		element: (
-			<ProtectedRoute>
-				<ViewContainers />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/edit-container/:id',
-		element: (
-			<ProtectedRoute>
-				<EditContainer />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/exported-containers',
-		element: (
-			<ProtectedRoute>
-				<ExportedContainers />
-			</ProtectedRoute>
-		),
-	},
-	{
-		path: '/*',
-		element: <Home />,
-	},
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<BrowserRouter>
+			{' '}
+			{/* Usamos BrowserRouter aquí */}
+			<Routes>
+				{' '}
+				{/* Usamos Routes en lugar de definir el router directamente */}
+				<Route path='/login' element={<Login />} />
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/create'
+					element={
+						<ProtectedRoute>
+							<CreateContainer />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/view-containers'
+					element={
+						<ProtectedRoute>
+							<ViewContainers />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/edit-container/:id'
+					element={
+						<ProtectedRoute>
+							<EditContainer />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/exported-containers'
+					element={
+						<ProtectedRoute>
+							<ExportedContainers />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path='/*' element={<Home />} /> {/* Ruta comodín para redirigir */}
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>,
 );
