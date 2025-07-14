ca0a824cff64ebd1c76bde0c21995809ef8e98a4
Juan Diaz <juan.diaz@caravela.coffee>
2025-07-14 16:15:07 -0500

    bug fix endpoints

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = 'https://bckcarcoltest.caravela.coffee/';
+//export const API_BASE_URL = 'https://bckcarcoltest.caravela.coffee/';
 //export const API_BASE_URL = 'https://bckcarcol.caravela.coffee/';
-//export const API_BASE_URL = 'http://localhost:8080/';
+export const API_BASE_URL = 'http://localhost:8080/';
 
 export const API_ENDPOINTS = {
 	PENDING_CONTAINERS: 'api/exports/getPendingContainers',
@@ -16,4 +16,10 @@ export const API_ENDPOINTS = {
 	BOOKING_AND_DATES: 'api/exports/addBookingAndDates',
 	SET_LOADED: 'api/exports/setLoaded',
 	VALIDATE_TOKEN: 'api/auth/validate-token',
+	// Comments endpoints
+	GET_USERNAME_FROM_TOKEN: 'api/exports/getUsernameFromToken',
+	GET_COMMENTS_BY_ICO: 'api/exports/getCommentsByIco',
+	ADD_COMMENT: 'api/exports/comment/add',
+	GET_UNREAD_COMMENTS: 'api/exports/comments/unread',
+	MARK_COMMENTS_AS_VIEWED: 'api/exports/comments/markAsViewed',
 };
