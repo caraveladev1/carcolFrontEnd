fa1dc962efc118e1ca39020ddbed07bbe0eedad8
Juan Diaz <juan.diaz@caravela.coffee>
2025-07-16 15:25:14 -0500

    changes in permissions

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,6 @@ import { ExportedContainers } from './pages/ExportedContainers';
 import { ManageUsers } from './pages/ManageUsers';
 import { AnnouncementsPage } from './pages/AnnouncementsPage';
 import { LoginMS } from './pages/loginMS/LoginMS';
-import { ProtectedRouteMS } from './pages/loginMS/ProtectedRouteMS';
 import { PermissionProtectedRoute } from './components/PermissionProtectedRoute';
 import { Unauthorized } from './pages/loginMS/Unauthorized';
 createRoot(document.getElementById('root')).render(
@@ -23,34 +22,32 @@ createRoot(document.getElementById('root')).render(
 				<Route path='/' element={<LoginMS />} />
 
 				{/* Rutas protegidas basadas en permisos */}
-				<Route element={<ProtectedRouteMS allowedRoles={['Admin', 'Viewer']} />}>
-					{/* Rutas que requieren containers.view */}
-					<Route element={<PermissionProtectedRoute requiredPermissions={['containers.view']} />}>
-						<Route path='/view-containers' element={<ViewContainers />} />
-						<Route path='/exported-containers' element={<ExportedContainers />} />
-						<Route path='/announcements/:ico?' element={<AnnouncementsPage />} />
-					</Route>
-
-					{/* Rutas que requieren tasks.view */}
-					<Route element={<PermissionProtectedRoute requiredPermissions={['tasks.view']} />}>
-						<Route path='/pending-task' element={<PendingTask />} />
-					</Route>
-
-					{/* Rutas que requieren containers.create/edit */}
-					<Route element={<PermissionProtectedRoute requiredPermissions={['containers.create']} />}>
-						<Route path='/create' element={<CreateContainer />} />
-					</Route>
-					<Route element={<PermissionProtectedRoute requiredPermissions={['containers.edit']} />}>
-						<Route path='/edit-container/:id' element={<EditContainer />} />
-					</Route>
-
-					{/* Rutas que requieren users.view */}
-					<Route element={<PermissionProtectedRoute requiredPermissions={['users.view']} />}>
-						<Route path='/admin/manage-users' element={<ManageUsers />} />
-					</Route>
-
-					<Route path='/unauthorized' element={<Unauthorized />} />
+				{/* Rutas que requieren containers.view */}
+				<Route element={<PermissionProtectedRoute requiredPermissions={['containers.view']} />}>
+					<Route path='/view-containers' element={<ViewContainers />} />
+					<Route path='/exported-containers' element={<ExportedContainers />} />
+					<Route path='/announcements/:ico?' element={<AnnouncementsPage />} />
 				</Route>
+
+				{/* Rutas que requieren tasks.view */}
+				<Route element={<PermissionProtectedRoute requiredPermissions={['tasks.view']} />}>
+					<Route path='/pending-task' element={<PendingTask />} />
+				</Route>
+
+				{/* Rutas que requieren containers.create/edit */}
+				<Route element={<PermissionProtectedRoute requiredPermissions={['containers.create']} />}>
+					<Route path='/create' element={<CreateContainer />} />
+				</Route>
+				<Route element={<PermissionProtectedRoute requiredPermissions={['containers.edit']} />}>
+					<Route path='/edit-container/:id' element={<EditContainer />} />
+				</Route>
+
+				{/* Rutas que requieren users.view */}
+				<Route element={<PermissionProtectedRoute requiredPermissions={['users.view']} />}>
+					<Route path='/admin/manage-users' element={<ManageUsers />} />
+				</Route>
+
+				<Route path='/unauthorized' element={<Unauthorized />} />
 				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
 		</BrowserRouter>
