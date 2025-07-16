1f0e936e3ac7bfc176e80216daded231a246e53a
Juan Diaz <juan.diaz@caravela.coffee>
2025-07-16 14:46:31 -0500

    last changes

---
+++ src/constants/legacy.js
@@ -0,0 +1,4 @@
+// Legacy constants that were originally in utils/consts.js
+// These should be gradually moved to more appropriate constant files
+
+export const placeholderFilter = ['office', 'exportMonth', 'packaging', 'contract', 'destinationPort'];
