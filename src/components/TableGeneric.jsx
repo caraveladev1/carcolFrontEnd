6df7bcf3989b5bbd6fe61d0eaea9945619f9e355
Juan Diaz <juan.diaz@caravela.coffee>
2025-07-17 11:08:27 -0500

    change in table cols

--- src/components/TableGeneric.jsx
+++ src/components/TableGeneric.jsx
@@ -6,11 +6,11 @@ export function TableGeneric({ headersTable, dataTable }) {
 
 	return (
 		<div className='overflow-x-auto mt-6 pb-6'>
-			<table className='min-w-full border-[2px] border-cafe' style={{ width: `${headersTable.length * 200}px` }}>
+			<table className='min-w-full border-[2px] border-cafe table-auto' >
 				<thead>
 					<tr className='bg-beige uppercase font-itf text-lg border-[2px] border-cafe h-[3rem]'>
 						{headersTable.map((header, index) => (
-							<th className='w-[4rem] h-[3rem] text-center text-verdeTexto border-[2px] border-cafe' key={index}>
+							<th className='h-[3rem] text-center text-verdeTexto border-[2px] border-cafe px-4 whitespace-nowrap' key={index}>
 								{t(header)}
 							</th>
 						))}
@@ -22,7 +22,7 @@ export function TableGeneric({ headersTable, dataTable }) {
 							<tr key={rowIndex} className='text-center border-[2px] border-cafe h-[3rem]'>
 								{headersTable.map((header, cellIndex) => (
 									<td
-										className='w-[4rem] h-[3rem] bg-verdeClaro/[.5] border-[2px] border-cafe font-itf text-beige px-4'
+										className='h-[3rem] bg-verdeClaro/[.5] border-[2px] border-cafe font-itf text-beige px-4 whitespace-nowrap'
 										key={cellIndex}
 									>
 										{row[header] || ''}
@@ -32,7 +32,7 @@ export function TableGeneric({ headersTable, dataTable }) {
 						))
 					) : (
 						<tr>
-							<td colSpan={headersTable.length} className='text-center py-4 bg-verdeClaro/[.5] font-itf text-beige'>
+							<td colSpan={headersTable.length} className='text-center py-4 bg-verdeClaro/[.5] font-itf text-beige px-4'>
 								No Data
 							</td>
 						</tr>
